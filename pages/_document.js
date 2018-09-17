e5c01e423fb4512052e936d49f226b578fdbba10
Randal Maia <randalmaia@gmail.com>
2018-09-17 20:06:13 -0300

    add language to html

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <html>
+      <html lang="en">
         <Head>
           <link href="https://fonts.googleapis.com/css?family=Montserrat:500,600" rel="stylesheet" />
           <meta name="viewport" content="width=device-width, initial-scale=1"/>
